354246f0fa67d7e4b9f5f4980946cd5e3000e410
Alkida Vaci <alkida.vaci@gmail.com>
2022-11-07 20:49:41 -0500

    Added functionality to get users news data from The Guardian Api

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,37 @@ $(function () {
     changeYear: true,
   });
 });
+
+// Declare API news key
+var apiNewsKey = 'db4aab48-4635-49ff-a537-39a673481b78';
+
+//  Function to get data
+function getNews(data) {
+
+}
+
+// Functionality to get users news data from the input league, team and date form api(userLeague, userTeam, userDate)
+var getApiNews = function (userLeague, userTeam, userDate) {
+  var apiUrl = 'https://content.guardianapis.com/search?q=' + userLeague + '&q=' + userTeam + '&from-date=' + userDate + '&api-key=' + apiNewsKey;
+  // var apiUrl ='https://content.guardianapis.com/search?q=champions%20league&q=juventus&from-date=2022-10-02&api-key=db4aab48-4635-49ff-a537-39a673481b78'
+
+  // Access open weather map resources across the network
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data)
+          // Function to get data
+          getNews(data)
+        });
+      } else {
+        // Input not correct, display status
+        alert('Error: ' + response.statusText);
+      }
+    })
+    // Alert error if api caller fail
+    .catch(function (error) {
+      alert('Unable to connect to The Guardian ');
+    });
+};
+// getApiNews()
